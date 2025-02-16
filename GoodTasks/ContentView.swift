8a1f58ce8b1df1ec7933363d88c8dd1a01d7de4d
Víctor Pimentel <victor@goodnotes.com>
2025-02-17 01:30:06 +0300

    05 - Add new task logic.

--- GoodTasks/ContentView.swift
+++ GoodTasks/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @Binding var tasks: [Task]
+    @State private var newTaskTitle: String = ""
 
     var body: some View {
         NavigationStack {
@@ -22,10 +23,27 @@ struct ContentView: View {
                             .foregroundColor(task.isCompleted ? .secondary : .primary)
                     }
                 }
+
+                HStack {
+                    CheckBox(isChecked: .constant(false))
+                        .hidden()
+
+                    TextField("Add new task", text: $newTaskTitle)
+                        .onSubmit {
+                            addTask()
+                        }
+                }
             }
             .listStyle(.plain)
             .navigationTitle("GoodTasks")
         }
+    }
+
+    func addTask() {
+        guard !newTaskTitle.isEmpty else { return }
+        let newTask = Task(title: newTaskTitle)
+        tasks.append(newTask)
+        newTaskTitle = ""
     }
 }
 
