fe633d5d01cad1dc4ea710c6e20a7c04470944b5
Víctor Pimentel <victor@goodnotes.com>
2025-02-17 00:55:30 +0300

    Initial Commit

---
+++ GoodTasks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GoodTasks
+//
+//  Created by Víctor Pimentel Rodríguez on 17/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GoodTasks/GoodTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodTasksApp.swift
+//  GoodTasks
+//
+//  Created by Víctor Pimentel Rodríguez on 17/2/25.
+//
+
+import SwiftUI
+
+@main
+struct GoodTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
